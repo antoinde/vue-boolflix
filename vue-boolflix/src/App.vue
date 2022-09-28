cd892e50070889ad53ed43a2ddce23abc17d1b4e
antoinde <antonello.indelicato98@gmail.com>
2022-09-28 19:20:05 +0200

    Aggiunto bootstrap

--- vue-boolflix/src/App.vue
+++ vue-boolflix/src/App.vue
@@ -1,7 +1,10 @@
 // API Key: 5c5ab0c379b45df5bb4161e9bbb65578
+// link API momentaneo: https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+fut
 <template>
   <div id="app">
-    
+    <div class="container">
+      
+    </div>
   </div>
 </template>
 
@@ -15,5 +18,6 @@ export default {
 </script>
 
 <style lang="scss">
+@import '@/assets/style/style.scss';
 
 </style>
