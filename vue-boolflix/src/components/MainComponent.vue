f91c63a114100f416af20c5ca4ba315d7de5c77e
antoinde <antonello.indelicato98@gmail.com>
2022-09-29 23:04:55 +0200

    Aggiunto img 404 not found nelle bandiere in error

--- vue-boolflix/src/components/MainComponent.vue
+++ vue-boolflix/src/components/MainComponent.vue
@@ -6,12 +6,13 @@
         <div class="col-12">
             <ul>
                 <li v-for="film in sentFilms" :key="film.id">
-                    <h6>{{ film.title }}</h6>
-                    <p>
-                        {{ film.original_title }} <br>
-                        {{ film.original_language }} <br>
-                        {{ film.vote_average }} <br>
-                    </p>
+                    <h6> <strong>TITOLO: </strong>  {{ film.title }}</h6>
+                    <p> <strong>Titolo originale:</strong>  {{ film.original_title }}</p>
+                    <div class="flag"> 
+                        <strong>Lingua originale: </strong> 
+                        <img :src="getFlag(film.original_language)" :alt="film.original_language" onerror="this.src = '//www.publicdomainpictures.net/pictures/280000/nahled/not-found-image-15383864787lu.jpg'"> 
+                    </div>
+                    <p> <strong>Voto: </strong> {{ film.vote_average }}</p>
                 </li>
             </ul>
         </div>
@@ -25,6 +26,17 @@ export default {
     name: 'MainComponent',
     props: {
         sentFilms: Array
+    },
+    methods: {
+        getFlag(initials_flag) {
+            console.log(initials_flag);
+            if(initials_flag==='en') initials_flag='gb';        
+            
+            let string = 'https://www.bandiere-mondo.it/data/flags/w702/' + initials_flag + '.webp';
+            console.log(string);
+            return string;
+        },
+        
     }
     
 }
@@ -35,6 +47,17 @@ li {
     list-style-type: none;
     border: 1px solid black;
     padding: 15px;
+}
+.flag {
+    img {
+        width: 30px;
+    }
+}
+.active {
+    display: block;
+}
+.disabled {
+    display: none;
 }
   
 </style>
