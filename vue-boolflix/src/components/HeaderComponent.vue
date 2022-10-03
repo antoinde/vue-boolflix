fa8b35b438232b18c5e16649acff5490ef757652
antoinde <antonello.indelicato98@gmail.com>
2022-10-03 18:58:43 +0200

    Aggiunte animazioni sull'hover e overview

--- vue-boolflix/src/components/HeaderComponent.vue
+++ vue-boolflix/src/components/HeaderComponent.vue
@@ -1,13 +1,15 @@
 <template>
-    <header class="row">
-        <div class="col-6 d-flex logo">
-            <img class="logo" src="@/assets/img/Netflix_logo.svg.png" alt="Logo Netflix">
-        </div>
-        <div class="col-6 d-flex justify-content-end">
-            <div class="search-area">
-                <span>Cerca qui: </span>
-                <input type="text"  v-model="textToSearch" @keyup.enter="sendRequest()">
-                <button @click="sendRequest()" >Cerca!</button>
+    <header>
+        <div class="row">
+            <div class="col-4 d-flex logo">
+                <img class="logo" src="@/assets/img/Netflix_logo.svg.png" alt="Logo Netflix">
+            </div>
+            <div class="col-4 d-flex justify-content-end">
+                <div class="search-area d-flex align-items-center">
+                    <span>Cerca qui: </span>
+                    <input type="text"  v-model="textToSearch" @keyup.enter="sendRequest()">
+                    <button @click="sendRequest()" >Cerca!</button>
+                </div>
             </div>
         </div>
     </header>
@@ -33,15 +35,24 @@ export default {
 </script>
   
 <style lang="scss" scoped>
+header {
+    padding: 20px 0px;
+    position:fixed;
+    top: 0px;
+    z-index: 999;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 0.667);
+}
 .search-area{
-    margin-top: 30px;
+    // margin-top: 30px;
 }
 input{
     margin: 0 10px;
 }
 .logo{
     height: 50px;
-    margin-top: 10px;
+    // margin-top: 10px;
 }
+
 </style>
   
--- vue-boolflix/src/components/MainComponent.vue
+++ vue-boolflix/src/components/MainComponent.vue
@@ -5,19 +5,20 @@
             <h1> FILM: </h1>
             <ul class="cards">
                 <li class="card" v-for="film in sentFilms" :key="film.id">
-                    <img :src="getPathImage(film.poster_path)" alt="cover film image" class="image_poster" 
-                    onerror="src='//lightwidget.com/wp-content/uploads/local-file-not-found.png'">
-                    <h6> <strong>TITOLO: </strong>  {{ film.title }}</h6>
-                    <p> <strong>ORIGINAL:</strong>  {{ film.original_title }}</p>
-                    <div class="flag"> 
-                        <strong>LINGUA: </strong> 
-                        <img :src="getFlag(film.original_language)" :alt="film.original_language" 
-                        onerror="this.src = '//www.publicdomainpictures.net/pictures/280000/nahled/not-found-image-15383864787lu.jpg'"> 
+                    <img :src="getPathImage(film.poster_path)" alt="cover film image" class="image_poster"
+                        onerror="src='//lightwidget.com/wp-content/uploads/local-file-not-found.png'">
+                    <p class="card-overview"> {{ film.overview }} </p>
+                    <h6> <strong>TITOLO: </strong> {{ film.title }}</h6>
+                    <p> <strong>ORIGINAL:</strong> {{ film.original_title }}</p>
+                    <div class="flag">
+                        <strong>LINGUA: </strong>
+                        <img :src="getFlag(film.original_language)" :alt="film.original_language"
+                            onerror="this.src = '//www.publicdomainpictures.net/pictures/280000/nahled/not-found-image-15383864787lu.jpg'">
                         <span> {{film.original_language}} </span>
                     </div>
-                    <p class="card-vote"> <strong>VOTO: </strong>  
-                        <!-- <i v-for="(star,index) in stars" :key="index" class="fa-solid fa-star"></i> -->
-                        <i v-for="n in 5" :key="n" class="fa-star" :class="( n <= film.vote_average/2 )?'fa-solid':'fa-regular'"></i>
+                    <p class="card-vote"> <strong>VOTO: </strong>
+                        <i v-for="n in 5" :key="n" class="fa-star"
+                            :class="( n <= Math.ceil(film.vote_average/2) )?'fa-solid':'fa-regular'"></i>
                     </p>
                 </li>
             </ul>
@@ -28,30 +29,30 @@
             <h1> SERIE TV: </h1>
             <ul class="cards">
                 <li class="card" v-for="serie in sentSeries" :key="serie.id">
-                    <img :src="getPathImage(serie.poster_path)" alt="cover series image" class="image_poster" 
-                    onerror="src='//lightwidget.com/wp-content/uploads/local-file-not-found.png'">
-                    <h6 class="card-title"> <strong>TITOLO: </strong>  {{ serie.name }}</h6>
-                    <p class="card-original-title"> <strong>ORIGINAL:</strong>  {{ serie.original_name }}</p>
-                    <div  class="card-language flag"> 
-                        <strong>LINGUA: </strong> 
-                        <img :src="getFlag(serie.original_language)" :alt="serie.original_language" 
-                        onerror="this.src = '//www.publicdomainpictures.net/pictures/280000/nahled/not-found-image-15383864787lu.jpg'"> 
+                    <img :src="getPathImage(serie.poster_path)" alt="cover series image" class="image_poster"
+                        onerror="src='//lightwidget.com/wp-content/uploads/local-file-not-found.png'">
+                    <h6 class="card-title"> <strong>TITOLO: </strong> {{ serie.name }}</h6>
+                    <p class="card-original-title"> <strong>ORIGINAL:</strong> {{ serie.original_name }}</p>
+                    <div class="card-language flag">
+                        <strong>LINGUA: </strong>
+                        <img :src="getFlag(serie.original_language)" :alt="serie.original_language"
+                            onerror="this.src = '//www.publicdomainpictures.net/pictures/280000/nahled/not-found-image-15383864787lu.jpg'">
                         <span> {{serie.original_language}} </span>
                     </div>
-                    <p class="card-vote"> <strong>VOTO: </strong> 
-                        <!-- <i v-for="(star,index) in stars" :key="index" class="fa-solid fa-star"></i> -->
-                        <i v-for="n in 5" :key="n" class="fa-star" :class="( n <= serie.vote_average/2 )?'fa-solid':'fa-regular'"></i> 
+                    <p class="card-vote"> <strong>VOTO: </strong>
+                        <i v-for="n in 5" :key="n" class="fa-star"
+                            :class="( n <= Math.ceil(serie.vote_average/2) )?'fa-solid':'fa-regular'"></i>
                     </p>
                 </li>
             </ul>
         </div>
-        
+
     </main>
 </template>
   
 <script>
-  
-  
+
+
 export default {
     name: 'MainComponent',
     data() {
@@ -66,8 +67,8 @@ export default {
     methods: {
         getFlag(initials_flag) {
             // console.log(initials_flag);
-            if(initials_flag==='en') initials_flag='gb';        
-            
+            if (initials_flag === 'en') initials_flag = 'gb';
+
             let string = 'https://www.bandiere-mondo.it/data/flags/w702/' + initials_flag + '.webp';
             // console.log(string);
             return string;
@@ -77,6 +78,8 @@ export default {
             // console.log(string);
             return string;
         },
+
+        /* 
         getStar(vote, iWantToReturn){
             // PASSA DA MAX VALUTATION 10 A MAX VALUTATION 5
             let new_vote=parseInt( Math.ceil(vote/2) );
@@ -91,12 +94,17 @@ export default {
                 return new_vote;
             }
         }
+        */
     }
-    
+
 }
 </script>
   
 <style lang="scss" scoped>
+main {
+    padding-top: 90px;
+}
+
 li {
     list-style-type: none;
     border: 2px solid rgb(23, 23, 23);
@@ -104,6 +112,7 @@ li {
     border-radius: 5px;
     padding: 8px;
 }
+
 .flag {
     img {
         width: 30px;
@@ -113,21 +122,44 @@ li {
 .image_poster {
     width: 100%;
 }
+
 .cards {
     display: flex;
     flex-wrap: wrap;
 }
+
 .card {
     width: calc(100% / 5 - 4px);
     background-color: black;
+    opacity: 0.7;
 
     strong {
         font-size: 0.8rem;
         color: grey;
     }
 }
-.card:hover {
 
+.card:hover {
+    opacity: 1;
+    transform: scale(103%);
+    transition: transform 200ms;
+}
+
+.card:hover .image_poster {
+    // togli l'immagine poster
+    display: none;
+}
+
+.card:hover .card-overview {
+    // rendi visibile overview
+    display: block;
+
+}
+
+.card-overview {
+    height: 320px;
+    overflow-y: auto;
+    display: none;
 }
 </style>
   
